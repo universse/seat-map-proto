163e437c65130dd4e342d104fad3c6992c0a9ab2
Ulrjch <phuoc317049@gmail.com>
2021-02-15 21:04:34 +0800

    Test

--- src/pages/api/getOverview.ts
+++ src/pages/api/getOverview.ts
@@ -1,4 +1,11 @@
-export default function getOverview(req, res) {
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+export default async function getOverview(req, res) {
+  await sleep(3000)
   res.setHeader('Cache-Control', 's-maxage=300, must-revalidate')
   res.status(200).json(require(`../../../data/overview/${req.body.id}.json`))
 }
