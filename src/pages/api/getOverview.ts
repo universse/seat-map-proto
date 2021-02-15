5e59858249cb9cfdacd540a0b70e3bca35949e3d
Ulrjch <phuoc317049@gmail.com>
2021-02-15 20:59:02 +0800

    Test caching

--- src/pages/api/getOverview.ts
+++ src/pages/api/getOverview.ts
@@ -1,3 +1,4 @@
 export default function getOverview(req, res) {
+  res.setHeader('Cache-Control', 's-maxage=300, must-revalidate')
   res.status(200).json(require(`../../../data/overview/${req.body.id}.json`))
 }
--- src/pages/api/getSeats.ts
+++ src/pages/api/getSeats.ts
@@ -1,3 +1,4 @@
 export default function getSeats(req, res) {
+  res.setHeader('Cache-Control', 's-maxage=300, must-revalidate')
   res.status(200).json(require(`../../../data/areas/${req.body.id}.json`))
 }
